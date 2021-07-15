2a2d88d5fb7bc24c205bc6efb08101b12d982f07
James Wordie <james.wordie@yahoo.com>
2021-07-15 21:38:18 +0100

    added the memoize to the fetch user to prevent over calling api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonplaceholder from '../api/jsonplaceholder';
+import _ from 'lodash';
 
 // export const fetchPosts = () => {
 //   return async dispatch => {
@@ -8,16 +9,21 @@ import jsonplaceholder from '../api/jsonplaceholder';
 //   };
 // };
 
+// equivelant versions
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// equivelant versions
+export const fetchUser = (id) => dispatch => {
+  _fetchUser(id, dispatch);
+};
 
-export const fetchUser = (id) => async dispatch => {
+// memoized fetch user function to prevent overly calling the api, eg each user has 10 posts
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data })
-};
+});
