4f9684c67b535f5a3e1f3bf6c91f76448b24851f
James Wordie <james.wordie@yahoo.com>
2021-07-16 08:42:55 +0100

    finished posts and users action creater

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import jsonplaceholder from '../api/jsonplaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts()); // redux thunk invokes the returned function and dispatches
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 // export const fetchPosts = () => {
 //   return async dispatch => {
 //     const response = await jsonplaceholder.get('/posts');
@@ -17,13 +25,18 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-// memoized fetch user function to prevent overly calling the api, eg each user has 10 posts
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data })
-});
+};
+
+// memoized fetch user function to prevent overly calling the api, eg each user has 10 posts
+
+
+// memoized fetch user function to prevent overly calling the api, eg each user has 10 posts
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
