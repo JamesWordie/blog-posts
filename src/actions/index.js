50d6de1cf623d453df4f759e4f506c6b2ea24f9a
James Wordie <james.wordie@yahoo.com>
2021-07-16 08:47:24 +0100

    Added in the lodash.chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,14 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   const userIds = _.uniq(_.map(getState().posts, 'userId'));
 
   userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // in the chain, the first agrument of next function call is passed in the background
+  // need to end with .value() which executes
+  // _.chain(getState().posts)
+  //   .map('userId')
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value();
 };
 
 // export const fetchPosts = () => {
